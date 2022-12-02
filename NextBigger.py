ac7440e4674fe0b2701b2dc5c5cc8557d01967a6
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-02 20:33:20 +0100

    para cuando i mayor que uno

--- NextBigger.py
+++ NextBigger.py
@@ -3,4 +3,9 @@ def next_bigger(n):
     i = len(s) - 1
     if n < 10 or i == 0:
         return - 1
+    while i >= 1 and s[i-1] >= s[i]:
+        i -= 1
+    if i == 0:
+        return -1      
+    temp, pos = len(s) - 1, s[i-1]
     
