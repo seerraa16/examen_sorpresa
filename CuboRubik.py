1b743cafa008ca6c2e11ec16cad808ba69e9ade5
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-02 20:25:45 +0100

    los ifs

--- CuboRubik.py
+++ CuboRubik.py
@@ -2,4 +2,13 @@ def cube(n):
     pass
     res = ''
     for i in range(1, n * 2 + 1):
-       
+        row = ''
+        if i <= n:
+            row += '/\\' * i + '_\\' * n
+            dist = n - i
+        else:
+            row += '\\/' * (n * 2 - i + 1) + '_/' * n
+            dist = i - n - 1
+        if 2 * n - i:
+            row += '\n'
+        res += dist * ' ' + row
