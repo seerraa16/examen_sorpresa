61cabdad83f078491f6ca784e6a30cfef0512591
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-02 20:29:54 +0100

    prueba 1 con copilot

--- AlphabetWar.py
+++ AlphabetWar.py
@@ -3,4 +3,9 @@ def alphabet_war(fight):
     right_side = {"m": 4, "q": 3, "d": 2, "z": 1}
     left_score = 0
     right_score = 0
+    for i in fight:
+        if i in left_side:
+            left_score += left_side[i]
+        elif i in right_side:
+            right_score += right_side[i]
     
