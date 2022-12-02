58ab0ad66e9cbc25634e6dfeffba1f14173dee91
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-02 20:22:44 +0100

    ayuda de copilot de copilot

--- RGB.py
+++ RGB.py
@@ -1,2 +1,14 @@
 def rgb(r, g, b):
     pass 
+    if r < 0:
+        r = 0
+    elif r > 255:
+        r = 255
+    if g < 0:
+        g = 0
+    elif g > 255:
+        g = 255
+    if b < 0:
+        b = 0
+    elif b > 255:
+        b = 255
